b2e95c7ceac8601c897e20d42584462d51c4a472
papamitra <papamitra@gmail.com>
2013-09-07 10:37:02 +0900

    RecipeR 追加

---
+++ Handler/Recipe.hs
@@ -0,0 +1,9 @@
+module Handler.Recipe where
+
+import Import
+
+getRecipeR :: Handler Html
+getRecipeR = error "Not yet implemented: getRecipeR"
+
+postRecipeR :: Handler Html
+postRecipeR = error "Not yet implemented: postRecipeR"
