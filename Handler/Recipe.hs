1a81d762ae254774c628bf44fd0325e04645cde6
papamitra <papamitra@gmail.com>
2013-09-07 12:53:32 +0900

    DBアクセステストコード

--- Handler/Recipe.hs
+++ Handler/Recipe.hs
@@ -3,7 +3,19 @@ module Handler.Recipe where
 import Import
 
 getRecipeR :: Handler Html
-getRecipeR = error "Not yet implemented: getRecipeR"
+getRecipeR = do
+  recipes <- runDB $ selectList [] [Desc ShipbuildPosted]
+  defaultLayout $ do
+    setTitle "recipe"
+    [whamlet|
+$if null recipes
+  <p>レシピはありません
+$else
+  <ul>
+    $forall Entity shipbuildId shipbuild <- recipes
+      <li>
+        #{show $ shipbuildPosted shipbuild}
+|]
 
 postRecipeR :: Handler Html
 postRecipeR = error "Not yet implemented: postRecipeR"
