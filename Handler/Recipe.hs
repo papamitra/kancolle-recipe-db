6f1459ed67d10ef1df862420cbf9f2be2a2ffc08
papamitra <papamitra@gmail.com>
2013-09-14 22:11:23 +0900

    formにselectFieldList追加

--- Handler/Recipe.hs
+++ Handler/Recipe.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Handler.Recipe where
+
+import Data.Text(pack)
 
 import Import
 
 getRecipeR :: Handler Html
 getRecipeR = do
-  (widget, enctype) <- generateFormPost recipeForm
   recipes <- runDB $ selectList [] [Desc ShipbuildPosted]
+  ships <- runDB $ selectList [] [Asc ShipId]
+  let shipList = [(pack $ shipName ship, shipid)| Entity shipid ship <- ships]
+  (widget, enctype) <- generateFormPost $ recipeForm shipList
   defaultLayout $ do
     setTitle "recipe"
     [whamlet|
@@ -25,9 +31,11 @@ $else
 
 |]
 
-data Recipe = Recipe Int 
-recipeForm = renderDivs $ Recipe
+data Recipe = Recipe Int ShipId
+recipeForm :: [(Text, Key Ship)] -> Html -> MForm Handler (FormResult Recipe, Widget)
+recipeForm shipList = renderDivs $ Recipe
              <$> areq intField "shipId" Nothing
+             <*> areq (selectFieldList shipList) "Ship" Nothing
 
 postRecipeR :: Handler Html
 postRecipeR = error "Not yet implemented: postRecipeR"
