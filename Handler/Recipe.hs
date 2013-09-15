b1e42683b7ebc4cc5022427e4c8f216e6a7933a9
papamitra <papamitra@gmail.com>
2013-09-15 10:28:22 +0900

    DBから値取る場合は selectFiledList ではなく selectField を使う

--- Handler/Recipe.hs
+++ Handler/Recipe.hs
@@ -6,19 +6,20 @@ import Data.Text(pack)
 
 import Import
 
+import Yesod.Form.Fields
+
 getRecipeR :: Handler Html
 getRecipeR = do
   recipes <- runDB $ selectList [] [Desc ShipbuildPosted]
-  ships <- runDB $ selectList [] [Asc ShipId]
-  let shipList = [(pack $ shipName ship, shipid)| Entity shipid ship <- ships]
-  (widget, enctype) <- generateFormPost $ recipeForm shipList
+  (widget, enctype) <- generateFormPost recipeForm
   defaultLayout $ do
     setTitle "recipe"
     [whamlet|
 $if null recipes
   <p>レシピはありません
     <form method=post action=@{RecipeR} entype=#{enctype}>
-    ^{widget}
+      ^{widget}
+      <input type=submit value="Submit">
 
 $else
   <ul>
@@ -28,14 +29,25 @@ $else
 
   <form method=post action=@{RecipeR} entype=#{enctype}>
     ^{widget}
+    <input type=submit value="submit">
 
 |]
 
-data Recipe = Recipe Int ShipId
-recipeForm :: [(Text, Key Ship)] -> Html -> MForm Handler (FormResult Recipe, Widget)
-recipeForm shipList = renderDivs $ Recipe
+data Recipe = Recipe Int ShipId deriving (Show)
+recipeForm :: Html -> MForm Handler (FormResult Recipe, Widget)
+recipeForm = renderDivs $ Recipe
              <$> areq intField "shipId" Nothing
-             <*> areq (selectFieldList shipList) "Ship" Nothing
+             <*> areq (selectField shipList) "Ship" Nothing
+  where
+    shipList = do
+      ships <- runDB $ selectList [] [Asc ShipId]
+      optionsPairs [(pack $ shipName ship, shipid)| Entity shipid ship <- ships]
 
+          
 postRecipeR :: Handler Html
-postRecipeR = error "Not yet implemented: postRecipeR"
+postRecipeR = do
+  ((result, widget), enctype) <- runFormPost $ recipeForm
+  case result of
+    FormSuccess recipe -> liftIO $ print (show recipe)
+    _ -> error "error"
+  redirect RecipeR
