5196fc186730b306fc05dca786fc5d713022fec2
papamitra <papamitra@gmail.com>
2013-10-12 14:45:56 +0900

    Devel.hs追加

---
+++ Handler/Devel.hs
@@ -0,0 +1,9 @@
+module Handler.Devel where
+
+import Import
+
+getDevelR :: Handler Html
+getDevelR = error "Not yet implemented: getDevelR"
+
+postDevelR :: Handler Html
+postDevelR = error "Not yet implemented: postDevelR"
