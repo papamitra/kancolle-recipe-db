250b125b9090331bc1061987e423e8b177cd0618
papamitra <papamitra@gmail.com>
2013-10-12 17:57:35 +0900

    Util追加

---
+++ Handler/Util.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Handler.Util where
+
+import Import
+
+import Data.Text(pack)
+import qualified Control.Monad as M
+import Data.Traversable
+import Data.Maybe
+import Text.Printf
+import Data.String(fromString)
+
+threesome :: [a] -> [[a]]
+threesome [] = []
+threesome xs = let (ys, rest) = splitAt 3 xs
+               in (ys : (threesome rest))
+                  
+data Recipe = Recipe {hqLv::Int,
+                      secId::ShipId,
+                      secLv::Int,
+                      fuel::Int,
+                      amm::Int,
+                      steel::Int,
+                      baux::Int,
+                      shipIds::[ShipId]} deriving (Show, Read)
+
+recipeForm :: Maybe Recipe -> Html -> MForm Handler (FormResult Recipe, Widget)
+recipeForm recipe extra = do
+             (vHqLv, fHqLv) <- mreq intField "司令Lv" (fmap hqLv recipe)
+             (vSecId, fSecId) <- mreq (selectField shipList) "秘書艦" (fmap secId recipe)
+             (vSecLv, fSecLv) <- mreq intField "秘書艦Lv" (fmap secLv recipe)
+             (vFuel, fFuel) <- mreq intField "燃料" (fmap fuel recipe)
+             (vAmm, fAmm) <- mreq intField "弾薬" (fmap amm recipe)
+             (vSteel, fSteel) <- mreq intField "鋼材" (fmap steel recipe)
+             (vBaux, fBaux) <- mreq intField "ボーキサイト" (fmap baux recipe)
+             ships <- M.sequence [mopt (selectField shipList) (fromString s) Nothing | i <- [1..6], let s = printf "建造%s" (show i)] -- printf "%d" i だとうまくいかない.
+             let ships3 = threesome ships
+             let inputValue = Recipe <$> vHqLv <*> vSecId <*> vSecLv <*> vFuel <*> vAmm <*> vSteel <*> vBaux <*> (catMaybes <$> sequenceA (map fst ships))
+
+             let widget = do
+                   [whamlet|
+                    #{extra}
+                    <div .control-group>
+                      <label .control-label>#{fvLabel fHqLv}
+                      <div .controls>^{fvInput fHqLv}
+                    <div .row>
+                      <div .span3>
+                        <label .control-label>#{fvLabel fFuel}
+                        <div .controls>^{fvInput fFuel}
+
+                        <label .control-label>#{fvLabel fAmm}
+                        <div .controls>^{fvInput fAmm}
+
+                      <div .span3>
+                        <label .control-label>#{fvLabel fSteel}
+                        <div .controls>^{fvInput fSteel}
+
+                        <label .control-label>#{fvLabel fBaux}
+                        <div .controls>^{fvInput fBaux}
+
+                    <div .control-group>
+                      <label .control-label>#{fvLabel fSecId}(Lv)
+                      <div .controls>^{fvInput fSecId}
+                        ^{fvInput fSecLv}
+
+                    <div .row>
+                      $forall ships <- ships3
+                        <div .span3>
+                          $forall (_, fShipId) <- ships
+                            <div .controls>^{fvInput fShipId}
+                   |]
+             return (inputValue, widget)
+  where
+    shipList = do
+      ships <- runDB $ selectList [] [Asc ShipId]
+      optionsPairs [(pack $ shipName ship, shipid)| Entity shipid ship <- ships]
