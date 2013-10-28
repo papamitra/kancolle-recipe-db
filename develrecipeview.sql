5fe2abc08b1cd3ad7848c94d613ecab609f60f03
papamitra <papamitra@gmail.com>
2013-10-28 13:12:50 +0900

    develrecipeview.sql

---
+++ develrecipeview.sql
@@ -0,0 +1,7 @@
+
+create view developrecipeview as
+select count(eq.id) equipcount, eq.name equipname, rs.id resourceid from develop dev
+left join equipment eq on eq.id=dev.equipment_id
+inner join ship sec on sec.id=dev.secretary
+inner join resource rs on dev.resource_id=rs.id
+group by eq.id;
