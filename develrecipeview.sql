f223ce5d6c7b9930e2daebe04428ffcd4047b8aa
papamitra <papamitra@gmail.com>
2013-11-02 15:10:51 +0900

    開発レシピ表示不具合修正

--- develrecipeview.sql
+++ develrecipeview.sql
@@ -4,4 +4,4 @@ select eq.id id, count(eq.id) equipcount, eq.name equipname, rs.id resourceid fr
 left join equipment eq on eq.id=dev.equipment_id
 inner join ship sec on sec.id=dev.secretary
 inner join resource rs on dev.resource_id=rs.id
-group by eq.id;
+group by rs.id, eq.id;
